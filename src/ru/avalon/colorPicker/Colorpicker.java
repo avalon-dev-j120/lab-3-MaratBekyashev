cb2be09648b4d6434f55796f52356764880414b9
JAVA <some@mail.ru>
2019-12-12 19:55:19 +0300

    Color Picker

--- src/ru/avalon/colorPicker/Colorpicker.java
+++ src/ru/avalon/colorPicker/Colorpicker.java
@@ -5,31 +5,35 @@ import java.awt.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-//import javax.swing.border.EmptyBorder;
-//import java.awt.event.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
 
-public class Colorpicker extends JFrame
+
+public class Colorpicker extends JFrame implements ChangeListener
 {
   final int WIDTH = 600, HEIGHT = 250;
   String hexColor;
+  String currentColor;
+  JPanel leftPanel, rightPanel;
+  JSlider[] sliders = new JSlider[3];
+  JSlider redSlider, greenSlider, blueSlider;
+  Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+  StringSelection stringSelection;
+
   // Constructor
   public Colorpicker() {
     this.setTitle("Color Picker");
-    //this.setResizable(false);
     this.setSize(WIDTH, HEIGHT);
     this.setMaximumSize(new Dimension(WIDTH+100, HEIGHT+ 100));
     this.setMinimumSize(new Dimension(WIDTH, HEIGHT));
     this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
     //
     Container pane = this.getContentPane();
     pane.setLayout(new GridLayout(1, 2, 20, 20));
 
-    JPanel leftPanel, rightPanel;
     // Левая часть окна с отображалкой цветов
     leftPanel = new JPanel();
-    JLabel label = new JLabel();
-    leftPanel.add(label);
     pane.add(leftPanel);
 
     // Правая часть окна со слайдерами
@@ -37,44 +41,49 @@ public class Colorpicker extends JFrame
     rightPanel.setLayout(new GridLayout(3, 2, 10, 10));
 
     rightPanel.add(new JLabel("Red"));
-    JSlider redSlider = new JSlider(0, 255);
-    //redSlider.
+    redSlider = new JSlider(0, 255);
     rightPanel.add(redSlider);
 
     rightPanel.add(new JLabel("Green"));
-    JSlider greenSlider = new JSlider(0, 255);
+    greenSlider = new JSlider(0, 255);
     rightPanel.add(greenSlider);
 
     rightPanel.add(new JLabel("Blue"));
-    JSlider blueSlider = new JSlider(0, 255);
+    blueSlider = new JSlider(0, 255);
     rightPanel.add(blueSlider);
 
-    JSlider[] sliders = {redSlider, greenSlider, blueSlider};
+    sliders[0] = redSlider;
+    sliders[1] = greenSlider;
+    sliders[2] = blueSlider;
     for (JSlider slider: sliders) {
       slider.setMajorTickSpacing(125);
       slider.setMinorTickSpacing(5);
       slider.setPaintTicks(true);
       slider.setPaintLabels(true);
-
-      slider.addChangeListener(new ChangeListener() {
-        public void stateChanged(ChangeEvent e) {
-          // меняем надпись
-          hexColor = "#";
-          String strValue;
-          for (JSlider slider: sliders) {
-            int value = slider.getValue();
-            strValue = Integer.toHexString(value);
-            strValue = strValue.length()==2?strValue:"0"+strValue;
-            hexColor += strValue;
-          }
-          //int value = ((JSlider)e.getSource()).getValue();
-          label.setText(hexColor);
-        }
-      });
+      slider.addChangeListener(this::stateChanged);
     }
 
     pane.add(rightPanel);
+    redSlider.setValue(127);
   }
 
+  @Override
+  public void stateChanged(ChangeEvent e) {
+    // меняем надпись
+    currentColor = "#";
+    String strValue;
+    Color color;
+    for (JSlider slider: sliders) {
+      int value = slider.getValue();
+      strValue = Integer.toHexString(value);
+      strValue = strValue.length()==2?strValue:"0"+strValue;
+      currentColor += strValue;
+    }
+    color = new Color(redSlider.getValue(), greenSlider.getValue(), blueSlider.getValue());
+    leftPanel.setBackground(color);
+    leftPanel.setToolTipText(currentColor);
+    stringSelection = new StringSelection(currentColor);
+    clipboard.setContents(stringSelection, null);
+  }
 
 }
